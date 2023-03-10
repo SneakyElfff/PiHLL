8af4fed4b6cbb2f4e75dcb367e1734e0340b7d97
Nina <alhimovich.nina@gmail.com>
2023-03-10 10:24:58 +0300

    Delete IV. Java/LW1 directory

--- IV. Java/LW1/src/main/java/LW1/service/CalculationService.java
+++
@@ -1,8 +0,0 @@
-package LW1.service;
-
-public interface CalculationService
-{
-    Double findMean(Double num1, Double num2, Double num3, Double num4);
-
-    Double findMedium(Double num1, Double num2, Double num3, Double num4);
-}
