c5907c3fe2a888388f914a3cf6467a8168543502
Nina <alhimovich.nina@gmail.com>
2023-03-24 09:23:00 +0300

    Update Lw1ApplicationTests.java

--- IV. Java/LW2/src/test/java/LW1/Lw1ApplicationTests.java
+++ IV. Java/LW2/src/test/java/LW1/Lw1ApplicationTests.java
@@ -10,66 +10,57 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Arrays;
 
 @SpringBootTest
-class Lw1ApplicationTests
-{
-	Double a = 1.0;
-	Double b = 3.0;
-	Double c = 5.0;
-	Double d = 7.0;
+class Lw1ApplicationTests {
+    private static final Double a = 1.0;
+    private static final Double b = 3.0;
+    private static final Double c = 5.0;
+    private static final Double d = 7.0;
 
-	CalculationService obj = new CalculationService() {
-		@Override
-		public Double findMean(Double num1, Double num2, Double num3, Double num4) {
-			return (num1 + num2 + num3 + num4) / 4;
-		}
+    private CalculationService obj = new CalculationService() {
+        @Override
+        public Double findMean(Double num1, Double num2, Double num3, Double num4) {
+            return (num1 + num2 + num3 + num4) / 4;
+        }
 
-		@Override
-		public Double findMedium(Double num1, Double num2, Double num3, Double num4) {
-			Double[] nums = new Double[] {num1, num2, num3, num4};
-			Arrays.sort(nums);
+        @Override
+        public Double findMedium(Double num1, Double num2, Double num3, Double num4) {
+            Double[] nums = new Double[]{num1, num2, num3, num4};
+            Arrays.sort(nums);
 
-			return ((nums[1] + nums[2]) / 2);
-		}
-	};
+            return ((nums[1] + nums[2]) / 2);
+        }
+    };
 
-	@Before
-	public void setUp()
-	{
-		a = 1.0;
-		b = 3.0;
-		c = 5.0;
-		d = 7.0;
+    @Before
+    public void setUp() {
+        CalculationService obj = new CalculationService() {
+            @Override
+            public Double findMean(Double num1, Double num2, Double num3, Double num4) {
+                return (num1 + num2 + num3 + num4) / 4;
+            }
 
-		CalculationService obj = new CalculationService() {
-			@Override
-			public Double findMean(Double num1, Double num2, Double num3, Double num4) {
-				return (num1 + num2 + num3 + num4) / 4;
-			}
+            @Override
+            public Double findMedium(Double num1, Double num2, Double num3, Double num4) {
+                Double[] nums = new Double[]{num1, num2, num3, num4};
+                Arrays.sort(nums);
 
-			@Override
-			public Double findMedium(Double num1, Double num2, Double num3, Double num4) {
-				Double[] nums = new Double[] {num1, num2, num3, num4};
-				Arrays.sort(nums);
+                return ((nums[1] + nums[2]) / 2);
+            }
+        };
+    }
 
-				return ((nums[1] + nums[2]) / 2);
-			}
-		};
-	}
+    @Test
+    public void testFindMean() {
+        Double result = 4.0;
 
-	@Test
-	public void testFindMean()
-	{
-		Double result = 4.0;
-		
-		Assert.assertEquals("(1+3+5+7) / 4 must be equal to 4", result, obj.findMean(a, b, c, d));
-	}
+        Assert.assertEquals("(1+3+5+7) / 4 must be equal to 4", result, obj.findMean(a, b, c, d));
+    }
 
-	@Test
-	public void testFindMedium()
-	{
-		Double result = 4.0;
+    @Test
+    public void testFindMedium() {
+        Double result = 4.0;
 
-		Assert.assertEquals("(3+5) / 2 must be equal to 4", result, obj.findMedium(a, b, c, d));
-	}
+        Assert.assertEquals("(3+5) / 2 must be equal to 4", result, obj.findMedium(a, b, c, d));
+    }
 
 }
