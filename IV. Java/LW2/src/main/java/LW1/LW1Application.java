6d73f804c82698c71013f9173174f1163f93e2d6
Nina <alhimovich.nina@gmail.com>
2023-03-24 09:22:28 +0300

    Update LW1Application.java

--- IV. Java/LW2/src/main/java/LW1/LW1Application.java
+++ IV. Java/LW2/src/main/java/LW1/LW1Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LW1Application
-{
-    public static void main(String[] args)
-    {
+public class LW1Application {
+    public static void main(String[] args) {
         SpringApplication.run(LW1.LW1Application.class, args);
     }
 }
